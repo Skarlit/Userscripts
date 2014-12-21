df867f1dee566a9c967942e570eb695f3300ad41
Ran Xie <rxxxie@gmail.com>
2014-12-21 04:47:08 -0800

    Seiga script

---
+++ nicoseiga.js
@@ -0,0 +1,28 @@
+// ==UserScript==
+// @name         Nico Seiga simplified
+// @namespace    https://github.com/Skarlit/Userscripts
+// @version      0.1
+// @description  Simpler view for nico seiga
+// @author       Skarlit
+// @match        http://seiga.nicovideo.jp/seiga/*
+// @grant        none
+// ==/UserScript==
+var illustWrapper = document.querySelector('.illust_wrapper');
+var illustMain = document.querySelector('.illust_main');
+var picFrame = document.createElement('iframe');
+illustMain.style.height = window.innerHeight + 'px';
+picFrame.src = document.querySelector('#illust_link').href;
+picFrame.frameborder = '0';
+picFrame.style.height = window.innerHeight + 'px';
+picFrame.style.width = '100%';
+picFrame.style.border = 'none';
+illustWrapper.style.width = '100%';
+illustWrapper.style.height = window.innerHeight + 'px';
+illustWrapper.innerHTML = '';
+illustWrapper.appendChild(picFrame);
+
+var style = document.createElement('style');
+
+style.innerHTML = '#related_info { margin: 0; width: 100%} .related_info_main {width: auto !important; float: initial !important;} #detail { margin: 0; width: 100%}';
+
+document.body.appendChild(style);
