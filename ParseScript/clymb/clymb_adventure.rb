63b0d7999e7d65d61420fae25cd939f571dac2f4
Skarlit <rxxxie@gmail.com>
2015-01-14 15:31:36 -0800

    Clymb parse done

--- ParseScript/clymb/clymb_adventure.rb
+++ ParseScript/clymb/clymb_adventure.rb
@@ -38,12 +38,12 @@ end
 class Parser
   attr_accessor :downloader, :homepage, :doc
   def initialize(cache=false, id=nil)
-    @downloader = Downloader.new
-    if cache
-      @homepage = @downloader.load_cache(id)
-    else
-      @homepage = @downloader.cache
-    end
+    # @downloader = Downloader.new
+    # if cache
+    #   @homepage = @downloader.load_cache(id)
+    # else
+    #   @homepage = @downloader.cache
+    # end
   end
 
   def extractAdventureLinks
@@ -71,20 +71,53 @@ class Parser
     end
   end
 
-  def parseAdventurePage(filepath)
+  def parseAll
+    if !Dir.exists?('json')
+      Dir.mkdir('json')
+    end
+    Dir['cache/*.cache'].each do |file| 
+      f = File.open(file)
+      if File.extname(f) == '.cache'
+        parseAdventurePage(f)
+      end
+    end
+  end
+
+  def parseAdventurePage(file)
     doc_data = {}
-    @doc = Nokogiri::HTML(File.read(filepath))
-    doc_data['trav-top'] = {
-      'title' => @doc.css('.trav-section-title.primary')[0].text
-    } 
+    id = File.basename(file, '.cache')
+    @doc = Nokogiri::HTML(File.read("cache/#{id}.cache"))
+    trip = {
+      'title' => @doc.css('.trav-section-title.primary')[0].text,
+      'subtitle' => @doc.css('.trav-section-title.sub')[0].text,
+      'price_person' => @doc.css('.trav-price')[0].text,
+      'price_person_msrp' => @doc.css('.trav-msrp')[0].text,
+      'duration' => @doc.css('.trav-base-font')[0].text,
+      'discount' => @doc.css('.trav-base-font')[1].text,
+      'short_description' => @doc.css('.trav-short-description').text,
+      'phone' => @doc.css('.icon-phone + .mobile-hide').text,
+      'email' => @doc.css('.icon-mail + .mobile-hide').text,
+      'images' => @doc.css('.trav-hero-img').map {|img| img.attributes['style'].value.split(';')[0].scan(/(?<=\').*(?=\')/)[0]},
+      'start_place' => @doc.css('.tg-unsocial > .layout-top-box > .location-label')[0].text,
+      'end_place' => @doc.css('.tg-unsocial > .layout-top-box > .location-label')[1].text,
+      'minimum_age' => @doc.css('.tg-unsocial > .layout-trans-top > .layout-controls:contains("MINIMUM AGE") .trav-husky-font').text,
+      'act_difficulty' =>  @doc.css('.tg-unsocial .trav-fitness-dot').length,
+      'passport' => @doc.css('.tg-unsocial').children.last.children[0].css('.trav-husky-font').text,
+      'accommodations' => @doc.css('.tg-unsocial').children.last.children[1].css('.trav-husky-font').text,
+      'trip_activities' => @doc.css('.media-vertical-list img').map { |img| img['alt']}
+    }
+    File.open("json/#{id}.json", 'w') do |file|
+      file.write(trip.to_json)
+    end
   end
 end
 
 
 def Runner
   p = Parser.new(true)
-  p.extractAdventureLinks
-  p.cacheAll
+  # p.extractAdventureLinks
+  # p.cacheAll
+  p.parseAll
 end
 
 
